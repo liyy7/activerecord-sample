468a983528499cce5a1d15b386265a30cc8658a7
Yongyu Lee <aleeyyu+01@gmail.com>
2015-03-18 03:36:47 +0900

    move require awesome_print into helper

--- base.rb
+++ base.rb
@@ -7,12 +7,9 @@ require 'bundler/setup'
 lib_path = File.expand_path('../lib', __FILE__)
 $LOAD_PATH.unshift(lib_path) unless $LOAD_PATH.include?(lib_path)
 
-require 'awesome_print'
 require 'helper'
 require 'databases'
 
 include Helper
-
-ActiveRecord::Base.logger = get_logger
 
 define_tables(JobPostingDatabase, OAuthDatabase) { require 'table_definitions' }
