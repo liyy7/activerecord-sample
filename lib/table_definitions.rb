53343bac3c302bfea2061a490bf60d47c28c0373
Yongyu Lee <aleeyyu+01@gmail.com>
2015-03-18 23:29:23 +0900

    use :invalid_ids to filter :belongs_to

--- lib/table_definitions.rb
+++ lib/table_definitions.rb
@@ -1,20 +1,17 @@
 # encoding: UTF-8
 
-# Add :unsigned_id as a valid association option
-module ActiveRecord::Associations::Builder
-  class Association
-    self.valid_options << :unsigned_id
-  end
-end
+# Add :invalid_ids as a valid association option
+ActiveRecord::Associations::Builder::Association.valid_options << :invalid_ids
 
 module BelongsToReflectionPatch
   def self.included(klass)
     klass.reflect_on_all_associations(:belongs_to).each do |reflection|
+      invalid_ids = reflection.options[:invalid_ids]
       klass.class_eval do
         define_method(reflection.name) do
-          instance_eval("#{reflection.name}_id") > 0 ? super : nil
+          invalid_ids.include?(instance_eval("#{reflection.name}_id")) ? nil : super()
         end
-      end if reflection.options[:unsigned_id]
+      end if invalid_ids.is_a?(Array) && !invalid_ids.empty?
     end
   end
 end
@@ -144,11 +141,11 @@ end
 
 class Location
   belongs_to :job_posting
-  belongs_to :country, { unsigned_id: true }
-  belongs_to :administrative_area, { unsigned_id: true }
-  belongs_to :locality_group, { unsigned_id: true }
-  belongs_to :locality, { unsigned_id: true }
-  belongs_to :ward, { unsigned_id: true }
+  belongs_to :country, { invalid_ids: [0] }
+  belongs_to :administrative_area, { invalid_ids: [0] }
+  belongs_to :locality_group, { invalid_ids: [0] }
+  belongs_to :locality, { invalid_ids: [0] }
+  belongs_to :ward, { invalid_ids: [0] }
 
   include BelongsToReflectionPatch
 end
